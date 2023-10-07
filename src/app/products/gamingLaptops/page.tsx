ec9246031832288be3c62d96c75cc51257d29fea
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-10-07 22:29:43 +0300

    feat:page for gaming laptops

---
+++ src/app/products/gamingLaptops/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Hero from "../../../components/Hero";
+import Products from "../../../components/Products";
+
+export default function GamingLaptops() {
+  return (
+    <div>
+      <Hero />
+      <Products />
+    </div>
+  );
+}
