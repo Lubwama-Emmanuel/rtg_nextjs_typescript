774fe8da6716877e165a01e48ab3da18124cbc6d
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-10-11 14:17:22 +0300

    feat:added text loader skeleton component

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Button from "../../../ui/Button";
 import { useSearchParams } from "next/navigation";
+import ImageLoader from "@/ui/ImageLoader";
+import TextLoader from "@/ui/TextLoader";
 
 type Laptop = {
   name: string;
@@ -39,7 +41,7 @@ export default function ProductOverview({ params }: ProductOverviewProps) {
 
       const data = await res.json();
       setLaptop(data.laptop);
-      // setIsLoading(false);
+      setIsLoading(false);
     }
 
     getLaptop(params.productId);
@@ -48,7 +50,9 @@ export default function ProductOverview({ params }: ProductOverviewProps) {
   const { name, mainImage, desc, otherImages } = laptop;
   const data = [mainImage, ...otherImages];
 
-  const [activeImage, setActiveImage] = useState(data[0]);
+  const [activeImage, setActiveImage] = useState(mainImage);
+
+  console.log(activeImage);
 
   function handleMouseEnter(newImage: string, index: number) {
     setActiveContainer(index);
@@ -57,51 +61,63 @@ export default function ProductOverview({ params }: ProductOverviewProps) {
 
   return (
     <div className="mx-auto mb-10 mt-20 grid max-w-[97%] items-center gap-10 sm:max-w-[90%] sm:grid-cols-[1fr_2fr]">
-      <div className="">
-        <div className=" flex items-center justify-center h-60">
-          <Image
-            src={activeImage}
-            alt={name}
-            width={224}
-            height={224}
-            className="w-80"
-          />
-        </div>
+      <div className="space-y-3">
+        {isLoading ? (
+          <ImageLoader height="60" svgHeight="10" />
+        ) : (
+          <div className=" flex items-center justify-center h-60">
+            <Image
+              src={activeImage}
+              alt={name}
+              width={224}
+              height={224}
+              className="w-80"
+            />
+          </div>
+        )}
         <div className="items-center flex justify-center">
-          {}
-          <div className="grid w-[90%] grid-cols-[repeat(auto-fit,_minmax(120px,_1fr))] ">
+          <div className="grid w-[98%] gap-3 grid-cols-[repeat(auto-fit,_minmax(120px,_1fr))] ">
             {data.map((laptop, index) => (
               <div
                 onMouseEnter={() => handleMouseEnter(laptop, index)}
                 className={
                   activeContainer === index
-                    ? `flex justify-center rounded-md bg-gradient-to-r from-emerald-600 to-emerald-800 hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-800`
-                    : `flex justify-center rounded-md  hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-800`
+                    ? `flex justify-center rounded-md h-14  hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-800`
+                    : `flex justify-center rounded-md h-14  hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-800`
                 }
                 key={Math.random()}
               >
-                {/* <img src={laptop} className="w-20" /> */}
-                <Image
-                  src={laptop}
-                  alt="gaming laptop"
-                  width={500}
-                  height={500}
-                  className="w-20"
-                />
+                {isLoading ? (
+                  <ImageLoader height="14" svgHeight="5" />
+                ) : (
+                  <Image
+                    src={laptop}
+                    alt="gaming laptop"
+                    width={500}
+                    height={500}
+                    className="w-20"
+                  />
+                )}
               </div>
             ))}
           </div>
         </div>
       </div>
       <div className="space-y-3">
-        <h2 className="text-3xl font-bold capitalize">{name}</h2>
-        <h3 className="text-2xl font-medium capitalize">details:</h3>
-        <p className="text-xl">{desc}</p>
-        <h3 className="text-xl ">Ugx 3,000,000</h3>
-        <div className="space-x-3">
-          <Button>add to cart</Button>
-          <Button>shop</Button>
-        </div>
+        {isLoading ? (
+          <TextLoader />
+        ) : (
+          <div>
+            <h2 className="text-3xl font-bold capitalize">{name}</h2>
+            <h3 className="text-2xl font-medium capitalize">details:</h3>
+            <p className="text-xl">{desc}</p>
+            <h3 className="text-xl ">Ugx 3,000,000</h3>
+            <div className="space-x-3">
+              <Button>add to cart</Button>
+              <Button>shop</Button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
